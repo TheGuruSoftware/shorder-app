c89a7e16a69647510612bb7c91ab44af5e725a3a
TheGuruSoftware <marcin.palubinski99@gmail.com>
2022-06-02 10:04:45 +0200

    Up

---
+++ prisma/migrations/20220602080310_asd/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "url" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "likes" TEXT NOT NULL,
+    "added" TEXT NOT NULL
+);
